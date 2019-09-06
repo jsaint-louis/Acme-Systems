e4f60e49e1aa19a6702fbe94ab0cb6ab467abc82
Jahson Saint-Louis <saintlouisjn@gmail.com>
2019-09-06 16:45:43 -0400

    Added the whatFor method to HelloDolly.

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,8 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Eclipse");
-
+	}
+		public static void whatFor() {
+			String Msg = "What For!";
+			System.out.println(Msg);
+		}
 
 	}
 
-}
+
